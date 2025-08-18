b4bbbc58f6b35a1b9653fa2d6ab13c413b278cb9
Ruturaj <ruturajnikam1010@gmail.com>
2025-08-18 13:05:53 +0530

    intial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-400 text-2xl'>I am a code motherfucker</h1>
+    </>
+  )
+}
+
+export default App
